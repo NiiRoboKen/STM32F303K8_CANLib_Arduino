0a02391f98f1c9a727aedf3ab06103cf72508134
kuromamekuromame88 <tei8sia@outlook.jp>
2026-05-18 17:10:25 +0900

    Update main.cpp

--- example/main.cpp
+++ example/main.cpp
@@ -4,32 +4,28 @@ STM32CAN can;
 
 void setup(){
   Serial.begin(115200);
-  if(can.begin(1000000, PA11_PA12)){
-    Serial.println("CAN Initialized");
-  } else {
-    Serial.println("CAN Initialization Failed");
-  }
+  can.begin(1000000, PA11_PA12);
 }
 
 void loop(){
-  CAN_msg_t rx;
-  if(can.receive(&rx)){
-    Serial.println(rx.id, HEX);
-  }
+    CAN_msg_t rx;
 
-  /*
-  CAN_msg_t msg;
+    if(can.receive(&rx)){
+        Serial.println(rx.id, HEX);
+        Serial.println(rx.data[0], HEX);
+        Serial.println(rx.data[1], HEX);
+    }
 
-  msg.id = 0x123;
-  msg.format = STANDARD_FORMAT;
-  msg.type = DATA_FRAME;
-  msg.len = 1;
-  msg.data[0] = 0x55;
+    CAN_msg_t msg;
 
-  can.send(msg);
-  delay(1000);
-  Serial.println(CAN1->ESR, HEX);
-  Serial.println(CAN1->TSR, HEX);
-  delay(1000);
-  */
+    msg.id = 0x123;
+    msg.format = STANDARD_FORMAT;
+    msg.type = DATA_FRAME;
+    msg.len = 2;
+    msg.data[0] = 0x11;
+    msg.data[1] = 0x22;
+
+
+    can.send(msg);
+    delay(1000);
 }
